ee0bf613684e3d768161bbcfa90d79b0bfad1699
ghelanikirtan <kyghelani2@gmail.com>
2025-05-16 18:11:53 +0530

    TASK 1 [16MAY2025] Completed: Library Management Systems [Designing full ORM from scratch]

--- 16MAY2025/T1_LIBRARY_MANAGEMENT_SYS/main.py
+++ 16MAY2025/T1_LIBRARY_MANAGEMENT_SYS/main.py
@@ -67,10 +67,40 @@ services.add_member(member1)
 services.add_member(member2)
 print("Added 2 Memberships")
 
+# Displaying all the books
+books = manage_books.get_all_books()
+print(f"BOOKS AVAILABLE TO BORROW:")
+for (book_id, book) in books:
+    print(f"""{book_id}. {book.title} by {book.author} [{book.genre} {book.publisher}]""")
+print('-'*75)
 
+# Displaying all the persons with memberships:
+members = manage_members.get_all_members()
+print(f"MEMBERS who can borrow books:")
+for (member_id, member) in members:
+    print(f"""{member_id}. {member.f_name} {member.l_name} | {member.email} | {member.phone}.""")
+print('-'*75)
 
+# Let's suppose 1st member is borrowing book with id 2
+transaction_id  = manage_transactions.borrow_books(
+    member_id=1,
+    book_id=2
+)
 
-
-
-    
-    
+# Report for the transaction
+print(f"Transaction Status!")    
+transaction_status = manage_transactions.check_status(trans_id=transaction_id)
+print(f"STATUS FOR transaction_id ({transaction_id}): {transaction_status}")    
+# 
+print(f"Report for the transaction!")
+trans_id, trans_details = manage_transactions.get_transaction_by_id(transaction_id)
+print(f"""TRANSACTION ID: {transaction_id}. 
+      BOOK ID: {trans_details.book_id}
+      MEMBER ID: {trans_details.member_id}
+      
+      ISSUE DATE: {trans_details.issue_date}
+      RETURN DATE: {trans_details.return_date}
+      
+      FINE AMOUNT: {trans_details.fine_amount}
+      STATUS: {trans_details.status}
+      """)
